72f8b4874d7d966095b9462b13a7de17856a7218
zhouzhipeng <admin@zhouzhipeng.com>
2021-01-11 21:17:59 +0800

    update

--- image-downloader/src/Main.java
+++ image-downloader/src/Main.java
@@ -1,5 +1,8 @@
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -8,6 +11,26 @@ public class Main {
         String htmlContent = new String(new URL(url).openStream().readAllBytes(), StandardCharsets.UTF_8);
 
         System.out.println(htmlContent);
+
+        String regex="http.+\\.jpg";
+
+
+        //正则
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(htmlContent);
+        while(matcher.find()){
+            String imgUrl=matcher.group();
+            System.out.println(imgUrl);
+
+            byte[] bytes = new URL(imgUrl).openStream().readAllBytes();
+            //将bytes 写入文件
+            //
+            String filename="/Users/zhouzhipeng/IdeaProjects/easy-java-beginners-course/image-downloader/images/"+
+                    System.currentTimeMillis()+".jpg";
+            new FileOutputStream(filename).write(bytes);
+
+        }
+
 
     }
 }
